05ae9bb2ab1ced57de93992bbcfdc7b8b240f5a5
Andrey <ckastrusshion@gmail.com>
2024-04-02 13:06:24 +0500

    add watch, cleanCss and build

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ import gulp from "gulp";
 import { deleteAsync } from "del";
 import gulpSass from "gulp-sass";
 import dartSass from "sass";
+import gulpRename from "gulp-rename";
+import cleanCss from "gulp-clean-css";
 const sass = gulpSass(dartSass);
+
 
 
 const paths = {
@@ -31,13 +34,27 @@ function clean() {
   return deleteAsync(["app/"]);
 }
 
-
 function styles() {
   return gulp.src(paths.styles.src)
     .pipe(sass())
+    .pipe(cleanCss())
+    .pipe(gulpRename({
+      basename: "style",
+      suffix: ".min"
+    }))
     .pipe(gulp.dest(paths.styles.dest))
 }
 
+function watch() {
+  gulp.watch(paths.styles.src, styles)
+}
+
+const build = gulp.series(clean, styles, watch);
+
 export { clean };
-export { styles };
+export { styles as css };
+export { watch };
+
+
+export default build;
 
